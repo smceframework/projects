a1a26323ffa8ebecf887a02fba823ebea199ba03
imadige <imadige@gmail.com>
2015-01-12 14:39:09 +0200

    update
    update

--- demo/main/view/site/index.php
+++ demo/main/view/site/index.php
@@ -3,11 +3,14 @@
     <div class="content">
       <hgroup>
         <h1>Welcome to SmceFramework!</h1>
-        <h2>High performing, Secure, Flexible and Simple PHP Framework Application</h2>
+        <h2>High performing, Secure, Flexible and Simple PHP / C++ Framework Application</h2>
       </hgroup>
       <div class="clear"></div>
-        <div class="read-on"><a href="http://www.smceframework.com" target="_blank" class="btn btn-large btn-block btn-inverse">Web</a>
-        <a href="https://github.com/imadige/smceframework-MVC" target="_blank" class="btn btn-large btn-block btn-danger">Github</a></div>
+        <div class="read-on">
+        <a href="http://www.smceframework.com" target="_blank" class="btn btn-large btn-block btn-inverse">Web</a>
+        <a href="https://github.com/smceframework/framework" target="_blank" class="btn btn-large btn-block btn-danger">Github Framework</a>
+        <a href="https://github.com/smceframework/projects" target="_blank" class="btn btn-large btn-block btn-warning">Github Projects</a>
+        </div>
       <div class="clear"></div>
     </div>
   </div>
