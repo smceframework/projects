660466383c06ee80f7a6ff0e3bed02d318db3f0e
root <imadige@gmail.com>
2015-01-30 22:33:57 +0200

    update

---
+++ project/main/components/CController.php
@@ -0,0 +1,19 @@
+<?php
+
+class CController
+{
+
+    public static $layout='//layouts/column1';
+	
+	public function beforeAction()
+	{
+		//before
+		
+	}
+	
+	public function afterAction()
+	{
+		//after
+	}
+
+}
